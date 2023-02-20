b66803a426e1cd183def20db9ef81e1aee0c9e30
Marcio Reis <dev.marcioreis@gmail.com>
2023-02-20 18:27:20 -0300

    Navbar finalizada

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const openBtn = document.querySelector('.open-btn')
+const closeBtn = document.querySelector('.close-btn')
+const menu = document.querySelector('.navbar')
+
+openBtn.addEventListener('click', () => {
+    menu.classList.add('active')
+    openBtn.style.display = 'none'
+    closeBtn.style.display = 'inline-block'
+})
+
+const closeMenu = () => {
+    menu.classList.remove('active')
+    closeBtn.style.display = 'none'
+    openBtn.style.display = 'inline-block'
+}
+
+closeBtn.addEventListener('click', closeMenu)
+
